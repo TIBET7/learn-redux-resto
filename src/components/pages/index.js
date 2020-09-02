d069fc743388cfffae69aa93b12e339bbc5df611
Dim <om1234777@gmail.com>
2020-09-02 12:23:56 +0300

    make lesson 3 homework

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,9 @@
 import MainPage from './main-page';
 import CartPage from './cart-page';
+import ItemPage from './cart-page';
 
 export {
     MainPage,
-    CartPage
+    CartPage,
+    ItemPage
 };
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,12 +1,16 @@
 export default class RestoService {
-    url = 'http://localhost:3001/menu';
+    _apiBase = 'http://localhost:3001';
+
+    async getResources(url) {
+        const res = await fetch(`${this._apiBase}${url}`)
     
-    getMenuItems = async () => {
-       const response = await fetch(this.url);
-       if (!response.ok) {
-           throw new Error('Server Error');
-       }
-       const result = await response.json();
-       return result;
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${url}` +
+            `, received ${res.status}`);
+        }
+        return await res.json();
     }
+        async getMenuItems () {
+            return await this.getResources('/menu/');
+        }   
 }
